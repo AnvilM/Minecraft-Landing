0664e64f6d7429ac7989b93e4e92c212c33ae353
Anvil <211anvil112@gmail.com>
2022-10-15 13:21:22 +0300

    Main
    s

---
+++ main.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+        <script src="scripts/scripts.js"></script>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="styles.css">
+        <title>OVERCRAFT</title>
+        <link type="image/x-icon" href="images\icon.png" rel="shortcut icon">
+    </head>
+    <body>
+        <div class="main">
+            <div class="divBanner">
+                <div class="banner">
+                    <div class="logo">
+                        <h1>OVERCRAFT</h1>
+                    </div>
+                    <div class="logoText">
+                        <p>Это один из лучших серверов своего рода</p>
+                        <p>Регистрируйся и играй прямо сейчас</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="rau">
+                <div class="regAndAuth">        
+                    <div onclick="Reg()" class="field reg">
+                        <div class="text">
+                            <h1>Нет аккаунта?</h1>
+                            <p>Так создайте его! <br>Это не займет много времени</p>
+                        </div>
+                    </div>
+                    <div onclick="Auth()" class="field auth">
+                    <div class="text">
+                            <h1>Уже есть аккаунт?</h1>
+                            <p>Так чего ты ждешь!? <br>Заходи и играй</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bb">
+                <div class="box">
+                    <div  class="field optimization">
+                        <div class="icon">
+                            <div class="sqr">
+                                <img class="whiteImg" src="images\sodiumicon.png">
+                            </div>
+                        </div>
+                        <div class="text">
+                            <h1>Оптимизация</h1>
+                            <p>На нашем сервере используются самые современные методы оптимизации игры, чтобы каждый мог играть на нашем сервере</p>
+                        </div>
+                    </div>
+                    <div class="field highOnline">
+                        <div class="icon">
+                                <div class="sqr">
+                                    <img class="whiteImg" src="images\onlineicon.png">
+                                </div>
+                            </div>
+                            <div class="text">
+                                <h1>Онлайн</h1>
+                                <p>Благодаря хорошей оптимизации на нашем сервере стабильно высокий онлайн без потери TPS</p>
+                            </div>
+                    </div>
+                    <div class="field launcher">
+                        <div class="icon">
+                            <div class="sqr">
+                                <img class="whiteImg" src="images\launchericon.png">
+                            </div>
+                        </div>
+                        <div class="text">
+                            <h1>Лаунчер</h1>
+                            <p>Специально для проекта нашими программистами был разработан лаунчер, для безопасного и удобного подключения</p>
+                        </div>
+                    </div>
+                    <div class="field comunity">
+                        <div class="icon">
+                                    <div class="sqr">
+                                        <img class="whiteImg" src="images\playersicon.png">
+                                    </div>
+                                </div>
+                                <div class="text">
+                                    <h1>Игроки</h1>
+                                    <p>Благодаря сплоченному комьюнити, игроки не редко вместе возводят огромные проекты, что порой занимает очень много времени и сил</p>
+                                </div>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        
+
+
+
+
+            
+        </div>
+
+
+
+
+    
+
+    </body>
+
+<footer>
+    <div class="text">
+        <div class="box">
+            <p>© Copyright Overcraft 2022-2023 Все права защищены</p>
+            <p>Контакты: <a href="#">Вконтакте</a>,<a href="#">Телеграм</a></p>
+        </div>
+        <p>Сотрудничество: <a href="mailto:procopenco2004@mail.ru">pvm@mail.ru</a></p>
+        <div class="heartBox">
+            <p>Made by&nbsp</p><p class="Anvil">Anvil</p> <p>&nbspwith</p>
+            <div class="hrt">
+                <div class="heart"></div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+</html>
---
+++ scripts/scripts.js
@@ -0,0 +1,2 @@
+function Reg(){location.href="../register/main.php"}
+function Auth(){location.href="../auth/main.php"}
